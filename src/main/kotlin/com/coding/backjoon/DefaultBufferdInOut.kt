7ea5f7f8944b047d6412f2b12f464bc625d1b561
pscheol <pscheol@gmail.com>
2025-06-26 09:25:37 +0900

    feat: add default Scanner

---
+++ src/main/kotlin/com/coding/backjoon/DefaultBufferdInOut.kt
@@ -0,0 +1,15 @@
+package com.coding.backjoon
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+
+    bw.close()
+    br.close()
+}
---
+++ src/main/kotlin/com/coding/backjoon/DefaultScanner.kt
@@ -0,0 +1,7 @@
+package com.coding.backjoon
+
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+
+}
